d6e15cc7ba6eae080f29af53d8f347e9f97c81be
Ben Schmidt <bmschmidt@gmail.com>
2018-08-20 22:52:39 -0400

    preprint draft

--- EDA_functions.R
+++ EDA_functions.R
@@ -8,9 +8,10 @@ into_major_nums = . %>%
 
 cipsjoin =  . %>% left_join(cips,by=c("CIPCODE"="CIPCode"))
 
-amacad = read_csv("AmacadCIPS.csv/2 CIP-HI Crosswalk (1987-pres)-Table 1.csv") %>% select(-starts_with("X"))
+amacad = read_csv("AmacadCIPS.csv/2 CIP-HI Crosswalk (1987-pres)-Table 1.csv") %>% select(-starts_with("X")) %>% mutate(CIPCODE = str_pad(CIPCODE, width = 7, pad = "0"))
 
 f = amacad %>% inner_join(cips, by = c("CIPCODE" = "CIP2010"))
+
 amadjusted = f %>% select(CIPCODE = CIPCode, Discipline, Bachelors, Doctoral)
 
 acadjoin = . %>% left_join(amadjusted)
